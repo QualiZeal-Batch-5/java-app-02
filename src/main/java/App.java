e89fdde192cf46b164cc43f083f57313549816b7
Likhitha Boorsu <twinkleboorsu1717@gmail.com>
2024-10-24 13:26:19 +0530

    addingfiles

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+package com.qualizeal.javaapp02;
+import com.qualizeal.javaapp02.operation.TwoNonDecimalNumbersDivision;
+ 
+public class App {
+	public static void main(String[] args) {
+		TwoNonDecimalNumbersDivision twoNonDecimalNumbersDivision = new TwoNonDecimalNumbersDivision();
+		double result1 = twoNonDecimalNumbersDivision.divide(40,20);
+		System.out.println(result1);
+	}
+}
+		
+		
