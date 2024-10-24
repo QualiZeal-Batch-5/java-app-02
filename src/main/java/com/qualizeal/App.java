7189376119986cc5f97066887daedbf781265087
varsha ch <varshachittaboina@gmail.com>
2024-10-24 11:55:53 +0530

    assignment 2 added

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,12 @@
-package com.qualizeal;
-
+package com.qualizeal.javaapp02;
+import com.qualizeal.javaapp02.operation.TwoNonDecimalNumbersDivision;
 public class App {
 	public static void main(String[] args) {
+		TwoNonDecimalNumbersDivision twoNonDecimalNumbersDivision = new TwoNonDecimalNumbersDivision();
+		double result1 = twoNonDecimalNumbersDivision.divide(10,3);
+		System.out.println(result1);
+		double result2 = twoNonDecimalNumbersDivision.divide(10,4);
+		System.out.println(result2);
 
 	}
 }
