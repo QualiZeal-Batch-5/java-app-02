f738881ac0aa047bfe063aaf1274d73b44deced0
Namratha <namrathareddie@gmail.com>
2024-10-24 11:58:28 +0530

    Division Solution

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,9 @@
 package com.qualizeal;
-
+import com.qualizeal.javaapp02.operation.TwoNonDecimalNumbersDivision;
 public class App {
 	public static void main(String[] args) {
-
+	TwoNonDecimalNumbersDivision twoNonDecimalNumbersDivision = new TwoNonDecimalNumbersDivision();
+		System.out.println(twoNonDecimalNumbersDivision.divide(10, 2));
+		System.out.println(twoNonDecimalNumbersDivision.divide(10, 5));
 	}
 }
