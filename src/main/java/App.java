ba2e083c3c150409861d131ba43152cd28e7ad9c
PRIYANKA VEERANALA <priyankanaidu2003@gmail.com>
2024-10-24 14:22:06 +0530

    division of two numbers

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+
+import com.qualizeal.javaapp02.operation.TwoNonDecimalNumbersDivision;
+public class App {
+	public static void main(String[] args){
+		TwoNonDecimalNumbersDivision twoNonDecimalNumbersDivision = new TwoNonDecimalNumbersDivision();
+		System.out.println(twoNonDecimalNumbersDivision.divide(10,3));
+	}
+}
