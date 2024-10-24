6b798fc94a138d022ec8ce7a0ea396d3f9f31d87
Aruna Kumari Pattapu <arunakumarip206@gmail.com>
2024-10-24 11:45:16 +0530

    Solution Divide.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,12 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp02.operation.TwoNonDecimalNumbersDivision;
+
 public class App {
 	public static void main(String[] args) {
+		TwoNonDecimalNumbersDivision twoNonDecimalNumbersDivision = new TwoNonDecimalNumbersDivision();
+		double result1 = twoNonDecimalNumbersDivision.divide(10,2);
+		System.out.println(result1);
 
 	}
 }
