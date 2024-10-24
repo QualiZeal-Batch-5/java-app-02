da89f7a52caa71cd3879596446588767e80ed635
Nikitha <nikithacheera25@gmil.com>
2024-10-24 11:54:15 +0530

    solution added.

---
+++ src/main/java/TwoNonDecimalNumbersDivision.java
@@ -0,0 +1,17 @@
+package com.qualizeal.javaapp02.operation;
+
+public class TwoNonDecimalNumbersDivision {
+	double result = 0;
+	public double divide(int num1,int num2) {
+		if (num1 < 0 || num2 < 0) {
+			result = -1.0;
+		}
+		else if ( num1 == 0 || num2 == 0) {
+			result = -2.0;
+		}
+		else {
+			result = (double)num1 / num2;
+		}
+		return result;
+	}
+}
