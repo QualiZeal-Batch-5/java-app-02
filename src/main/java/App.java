eb5ecf824f3e23fa35ed549efee9096a04c8519a
Sriya <sriyagunga05@gmail.com>
2024-10-24 13:30:25 +0530

    addingfiles

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+package com.qualizeal;
+import com.qualizeal.javaapp02.operation.TwoNonDecimalNumbersDivision;
+public class App {
+	public static void main(String[] args) {
+		TwoNonDecimalNumbersDivision twodecimalnumbersdivision = new TwoNonDecimalNumbersDivision();
+		twodecimalnumbersdivision.divide(10,2);
+
+	}
+}
