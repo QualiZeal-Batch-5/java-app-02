3bb2ff58ec7ec758074ee7521c32a125d789cd6c
Dhana <dhanaposimsetti89@gmail.com>
2024-10-24 11:28:40 +0530

    Added Code

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,12 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp02.operation.TwoNonDecimalNumbersDivision;
+
 public class App {
 	public static void main(String[] args) {
-
+		TwoNonDecimalNumbersDivision twoNonDecimalNumbersDivision = new TwoNonDecimalNumbersDivision();
+		System.out.println(twoNonDecimalNumbersDivision.divide(10, 3));
+		System.out.println(twoNonDecimalNumbersDivision.divide(10, 0));
+		System.out.println(twoNonDecimalNumbersDivision.divide(-10, 2));
 	}
 }
