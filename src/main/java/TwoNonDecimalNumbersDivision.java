c1d67f8b95bb7dd031540677e73f6ff71cd2af06
Mounika Sri <mounikasriavidi@gmail.com>
2024-10-24 11:53:55 +0530

    Added TwoNonDecimalNumbersDivision Source file

---
+++ src/main/java/TwoNonDecimalNumbersDivision.java
@@ -0,0 +1,16 @@
+package com.qualizeal.javaapp02.operation;
+public class TwoNonDecimalNumbersDivision {
+	public double divide(int num1,int num2){
+		double result;
+		if(num1 < 0 || num2 < 0) {
+			return -1.0;
+		}
+		else if(num1 == 0 || num2 == 0){
+			return -2.0;
+		}
+		else{
+			result =  ((double) num1 / (double) num2);
+		}
+			return result;
+	}
+}
