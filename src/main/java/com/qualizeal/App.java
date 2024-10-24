7890ef1afb4f0a7c8fb94d767fdaae8049c6a67b
Bhavya Reddy <karribhavyareddy6@gmail.com>
2024-10-24 11:48:28 +0530

    Added Solution

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,10 @@
-package com.qualizeal;
-
+package com.qualizeal.javaapp02;
+ 
+import com.qualizeal.javaapp02.operation.TwoNonDecimalNumbersDivision;
 public class App {
 	public static void main(String[] args) {
-
+		TwoNonDecimalNumbersDivision twoNonDecimalNumbersDivision = new TwoNonDecimalNumbersDivision();
+		double result1 = twoNonDecimalNumbersDivision.divide(10,2);
+		System.out.println(result1);
 	}
 }
