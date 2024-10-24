caa46f369e3df9abb6a5434ef2c0938a76fd8034
SnehareddyPasham <snehalathareddy3002@gmail.com>
2024-10-24 12:00:52 +0530

    added java assignmennt

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,9 @@
 package com.qualizeal;
-
+import com.qualizeal.javaapp02.operation.TwoNonDecimalNumbersDivision;
 public class App {
 	public static void main(String[] args) {
+		TwoNonDecimalNumbersDivision twonondecimalnumberssum = new TwoNonDecimalNumbersDivision();
+		System.out.println(twonondecimalnumberssum.divide(10,3));
 
 	}
 }
